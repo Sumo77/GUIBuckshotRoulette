b0e23004aacd7e5b45f8ad956a64ca1b91ee7493
Sumo77 <prummer555@gmail.com>
2025-05-30 19:39:35 +1200

    Created and embedded a database for scoreboards !

---
+++ PDCProject2GUI/src/GameDatabase/DatabaseManager.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package GameDatabase;
+
+/**
+ *
+ * @author Prumm
+ */
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseManager {
+    private static final String USER_NAME = "BuckShotRoulette"; //your DB username
+    private static final String PASSWORD = "pdc"; //your DB password
+    private static final String URL = "jdbc:derby:BSRScoreDatabase_Ebd; create=true";  //url of the DB host
+    
+    Connection conn;
+
+    public DatabaseManager() {
+        establishConnection();
+    }
+
+    public static void main(String[] args) {
+        DatabaseManager dbManager = new DatabaseManager();
+        System.out.println(dbManager.getConnection());
+    }
+
+    public Connection getConnection() {
+        return this.conn;
+    }
+
+    //Establish connection
+    public void establishConnection() {
+        if (this.conn == null) {
+            try {
+                conn = DriverManager.getConnection(URL, USER_NAME, PASSWORD);
+                System.out.println(URL + " Get Connected Successfully ....");
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+
+    public void closeConnections() {
+        if (conn != null) {
+            try {
+                conn.close();
+            } catch (SQLException ex) {
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    
+}
