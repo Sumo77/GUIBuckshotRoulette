9f56dced0d5809e370c81a66bcf2a560731dce9b
Sumo77 <prummer555@gmail.com>
2025-05-31 14:02:38 +1200

    Updated some comments

--- PDCProject2GUI/src/BSRGameDatabase/WinTableManager.java
+++ PDCProject2GUI/src/BSRGameDatabase/WinTableManager.java
@@ -12,7 +12,6 @@ package BSRGameDatabase;
 import java.sql.*;
 
 public class WinTableManager { // Manages the WinTable Reading / Writing to and from the Database
-    // Migrate each method
     
     private final Connection conn; // Database !
     
@@ -25,57 +24,57 @@ public class WinTableManager { // Manages the WinTable Reading / Writing to and 
     public void createWinTable() { // Create the winsTable if not yet already exists
         try {
             PreparedStatement prepStatement = conn.prepareStatement("SELECT TABLENAME FROM SYS.SYSTABLES WHERE TABLENAME = 'WINS_TABLE'");
-            ResultSet result = prepStatement.executeQuery();
+            ResultSet result = prepStatement.executeQuery(); // Try to select the WinsTable from the systems tables
             
-            if (result.next()) {
-                System.out.println("Table exists");
-            } else {
+            if (result.next()) { // If can select the table,
+                System.out.println("Table exists"); // Table exists !
+            } else { // Nothing selected, table does not exist, so create !
                 prepStatement = conn.prepareStatement("CREATE TABLE WINS_TABLE (Username VARCHAR(255) PRIMARY KEY, Wins INT)");
                 prepStatement.executeUpdate(); // Create winsTable that stores username and number of wins
             }
             
             prepStatement.close(); // End statement nicely
             
-        } catch (SQLException ex) { // Error if failed to create table
-             System.out.println("Failed to create Wins_Table: " + ex.getMessage()); 
+        } catch (SQLException ex) { // Error if failed to create table or check if table already existed
+             System.out.println("Failed to check for/create Wins_Table: " + ex.getMessage()); 
         }
     }
     
     public int retrievePlayerWins(String username) { // Retrieve from the winTable and return the player's score based on username
         try {
             PreparedStatement prepStatement = conn.prepareStatement("SELECT WINS FROM WINS_TABLE WHERE USERNAME = ?");
-            prepStatement.setString(1, username);
+            prepStatement.setString(1, username); // insert username into SQL statement
             
-            ResultSet result = prepStatement.executeQuery();
+            ResultSet result = prepStatement.executeQuery(); // Select wins from the winsTable based on username provided
             
-            if (result.next()) {
-                int wins = result.getInt("WINS");
+            if (result.next()) { // If was able to select username (there is a row),
+                int wins = result.getInt("WINS"); // Get the wins of the row
                 
-                result.close();
+                result.close(); // End statements nicely (before return ends method)
                 prepStatement.close();
                 
-                return wins;
+                return wins; // Return number of wins for user !
             }
             
-            result.close();
+            result.close(); // End statements nicely
             prepStatement.close();
                 
         } catch (SQLException ex) { 
             System.out.println("Failed to create retrieve wins from the Wins_Table: " + ex.getMessage());
         }
         
-        return 0;
+        return 0; // No existing wins or user found, return 0! - This is fallback return, should always be a 0
     }
     
     public void updatePlayerWins(String username, int wins) { // Update the players wins in winTable based on username
         try {
             PreparedStatement prepStatement = conn.prepareStatement("UPDATE WINS_TABLE SET WINS = ? WHERE USERNAME = ?");
-            prepStatement.setInt(1, wins);
-            prepStatement.setString(2, username);
+            prepStatement.setInt(1, wins); // Number of users new wins for SQL
+            prepStatement.setString(2, username); // Username for SQL
             
-            prepStatement.executeUpdate();
+            prepStatement.executeUpdate(); // Update the wins of a specific user with provided values
             
-            prepStatement.close();
+            prepStatement.close(); // End statement nicely
             
         } catch (SQLException ex) {
             System.out.println("Failed to update player wins in the Wins_Table: " + ex.getMessage());
@@ -86,16 +85,16 @@ public class WinTableManager { // Manages the WinTable Reading / Writing to and 
     public boolean checkPlayerExists(String username) { // Check if player exists in winTable already based on username
         try {
             PreparedStatement prepStatement = conn.prepareStatement("SELECT USERNAME FROM WINS_TABLE WHERE USERNAME = ?");
-            prepStatement.setString(1, username);
+            prepStatement.setString(1, username); // Username for SQL
             
-            ResultSet result = prepStatement.executeQuery();
+            ResultSet result = prepStatement.executeQuery(); // Try to find username in winsTable, provided username
             
-            if (result.next()) { // Check if there is a row returned/to select/move to
-                result.close();
+            if (result.next()) { // Check if there is a row returned to select/move to,
+                result.close(); // End statements nicely (before return)
                 prepStatement.close();
                 return true; // If there is, Player found !
             } else {
-                result.close();
+                result.close(); // End statements nicely (before return)
                 prepStatement.close();
                 return false; // If not, Player not found !
             }
@@ -111,12 +110,12 @@ public class WinTableManager { // Manages the WinTable Reading / Writing to and 
     public void addNewPlayer(String username) { // Add new player to winTable
         try {
             PreparedStatement prepStatement = conn.prepareStatement("INSERT INTO WINS_TABLE (USERNAME, WINS) VALUES (?, ?)");
-            prepStatement.setString(1, username); // Use / save under entered username
-            prepStatement.setInt(2, 0); // 0 wins as is new player!
+            prepStatement.setString(1, username); // Username to give to SQL
+            prepStatement.setInt(2, 0); // 0 wins as is new player! give to SQL
             
-            prepStatement.executeUpdate();
+            prepStatement.executeUpdate(); // Insert new users values (username + 0 wins) into the winsTable to keep record
 
-            prepStatement.close();
+            prepStatement.close(); // End statement nicely
             
         } catch (SQLException ex) {
             System.out.println("Failed to add New Player to the Wins_Table: " + ex.getMessage());
@@ -127,19 +126,19 @@ public class WinTableManager { // Manages the WinTable Reading / Writing to and 
         try {
             PreparedStatement prepStatement = conn.prepareStatement("SELECT USERNAME, WINS FROM WINS_TABLE ORDER BY WINS DESC FETCH FIRST 10 ROWS ONLY");
             
-            ResultSet result = prepStatement.executeQuery();
+            ResultSet result = prepStatement.executeQuery(); // Get the Top 10 (or less) players (username + wins) organised by decending num of wins
             
-            int numRank = 1;
+            int numRank = 1; // Number them from 1 - 10 for leader stats !
             
-            while(result.next()) {
-                String username = result.getString("USERNAME");
-                int wins = result.getInt("WINS");
+            while(result.next()) { // Cycle through each row of the selected from the statement above ^
+                String username = result.getString("USERNAME"); // Save username to var
+                int wins = result.getInt("WINS"); // Save win to var
                 
-                System.out.println(numRank + ": " + username + " | " + wins);
-                numRank++;
+                System.out.println(numRank + ": " + username + " | " + wins); // Print out info
+                numRank++; // Increase rank for next person
             }
 
-            prepStatement.close();
+            prepStatement.close(); // End statement nicely
             
         } catch (SQLException ex) {
             System.out.println("Failed to add New Player to the Wins_Table: " + ex.getMessage());
